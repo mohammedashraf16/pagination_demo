67cebee4a2de1b5acfc431a7b16a364e1e72e7f8
Mohammed Ashraf <141479986+mohammedashraf16@users.noreply.github.com>
2025-12-03 23:40:44 +0200

    Remove unused flutter_screenutil import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pagination_demo/features/products/views/home_view.dart';
 
 void main() {
